74bbf675ac734e1dc493c08988ead53200013142
Jeremy <jeremy@colectica.com>
2023-12-01 11:34:24 +0100

    Cache python packages

--- build/appveyor-install-dependencies.ps1
+++ build/appveyor-install-dependencies.ps1
@@ -6,12 +6,3 @@ $output = "graphviz-2.38.zip"
 Invoke-WebRequest -Uri $url -OutFile $output
 
 7z x graphviz-2.38.zip -y -o"graphviz"
-
-ls graphviz
-
-python -V
-python -m pip install --upgrade pip
-# Download Sphinx
-pip install sphinx
-pip install sphinx_rtd_theme
-pip install Pygments
